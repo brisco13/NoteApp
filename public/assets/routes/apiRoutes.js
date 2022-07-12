d68d82537b76eaa35765926872603070d00f9052
Brisco Arechederra <brisco.arch@gmail.com>
2022-07-12 12:19:35 -0600

    get notes now works

--- public/assets/routes/apiRoutes.js
+++ public/assets/routes/apiRoutes.js
@@ -1,15 +1,22 @@
 const router = require('express').Router();
 const fs = require("fs");
-//const uuidv1 = require("uuid");
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all notes
 router.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
+    fs.readFile(`db/db.json`, (err, data) => {
+        let existingNotes = [];
+        if (err) {console.log(err)};
+
+        existingNotes = JSON.parse(data);
+        console.log(existingNotes);
+        res.send(existingNotes);
+    })
   });
   
-  // POST Route for a new UX/UI tip
+  // POST Route for a new note
 router.post('/notes', (req, res) => {
-    console.info(`${req.method} request received to add a tip`);
+    console.info(`${req.method} request received to add a note`);
     console.log(req.body);
   });
   
